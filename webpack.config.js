9ab8eeb86d465be92d2804737a67522b99379227
mortuie <jimmybob-leon@hotmail.co.uk>
2018-03-18 00:07:01 +0000

    Changed the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/KatexComponent.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
+    filename: 'KatexComponent.js',
     libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
   },
   module: {
